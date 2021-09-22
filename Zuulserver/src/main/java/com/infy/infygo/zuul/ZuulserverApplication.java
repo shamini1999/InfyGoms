7f1c0ce2d211101c134b01bbfbd37fac5e57d800
shamini1999 <56649108+shamini1999@users.noreply.github.com>
2021-09-22 19:36:24 +0530

    Add files via upload

---
+++ Zuulserver/src/main/java/com/infy/infygo/zuul/ZuulserverApplication.java
@@ -0,0 +1,19 @@
+package com.infy.infygo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+	
+
+public class ZuulserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserverApplication.class, args);
+	}
+
+}
---
+++ Zuulserver/src/test/java/com/infy/infygo/zuul/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.infygo.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
